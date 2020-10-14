d9e4816adc8f100274fd402b610018fea652f8f0
prat96 <pratheekb96@gmail.com>
2020-10-14 22:59:24 +0200

    First exercise commit

---
+++ basics/ch0/chapter0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* LET'S FUCKING DO THIS!! */
+int main(int argc, char *argv[]){
+
+	int distance = 100;
+
+	// maaaan, ok fine.
+	printf("You are %d miles away. \n", distance);
+
+	return 0;
+}
